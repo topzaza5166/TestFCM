d1330b7621b9278b210f14c929c023a061b3d67c
topza5166 <topzaza5166@gmail.com>
2017-07-27 04:28:12 +0700

    My Commit

--- app/src/main/java/com/example/topza/testfcmtoamata/activity/MainActivity.java
+++ app/src/main/java/com/example/topza/testfcmtoamata/activity/MainActivity.java
@@ -8,6 +8,7 @@ import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.SpinnerAdapter;
 import android.widget.Toast;
 
 import com.example.topza.testfcmtoamata.manager.HttpManager;
@@ -27,68 +28,77 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.Callable;
 
+import rx.Observable;
 import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
 import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String AUTH_KEY = "key=AAAAeHhUqSU:APA91bFeajqq_ilkOhXA1RjJNd3DIj7t4CdALH4VWVkxeT_SkHhUsbQp-8t8z-g5r0-Jy3VNP-exaXn8-XHT9d_r-pnbUTfKQYRj_500Ib3TLU1YfAOEmz3BLuS9LJiC1k8u-uU-v4sh";
+
     ActivityMainBinding binding;
-    List<String> listSpinner = new ArrayList<>();
+
+    private ArrayAdapter spinnerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
+        spinnerAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item);
+        binding.spinnerTopic.setAdapter(spinnerAdapter);
         getBoardList();
+
         binding.buttonSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                pushNotification((String) binding.spinnerTopic.getSelectedItem(),
-                        binding.editTitle.getText().toString(),
-                        binding.editMessage.getText().toString());
+                final ProgressDialog dialog = ProgressDialog.show(MainActivity.this, "", "Please Wait", true, false);
+                Observable.fromCallable(new Callable<String>() {
+                    @Override
+                    public String call() throws Exception {
+                        String topic = (String) binding.spinnerTopic.getSelectedItem();
+                        String title = binding.editTitle.getText().toString();
+                        String message = binding.editMessage.getText().toString();
+                        return pushNotification(topic, title, message);
+                    }
+                }).subscribeOn(Schedulers.io()).subscribe(new Action1<String>() {
+                    @Override
+                    public void call(String s) {
+                        Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+                        dialog.cancel();
+                    }
+                });
             }
         });
     }
 
     private void getBoardList() {
-        final ProgressDialog dialog = ProgressDialog.show(this, "", "Please Wait", true, false);
         String authHeader = "Basic " + Base64.encodeToString("admin@hotmail.com:admin".getBytes(), Base64.NO_WRAP);
         rx.Observable<BoardResponseDao> observable = HttpManager.getInstance().getService().getBoard(authHeader, Base64.NO_WRAP);
         observable.subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .unsubscribeOn(Schedulers.io())
-                .subscribe(new Subscriber<BoardResponseDao>() {
+                .doOnNext(new Action1<BoardResponseDao>() {
                     @Override
-                    public void onCompleted() {
-                        binding.spinnerTopic.setAdapter(new ArrayAdapter<>(
-                                MainActivity.this,
-                                android.R.layout.simple_spinner_dropdown_item,
-                                listSpinner)
-                        );
-                        dialog.cancel();
+                    public void call(BoardResponseDao boardResponseDao) {
+                        spinnerAdapter.addAll(boardResponseDao.getData());
+                        spinnerAdapter.notifyDataSetChanged();
                     }
-
+                })
+                .doOnError(new Action1<Throwable>() {
                     @Override
-                    public void onError(Throwable e) {
-                        Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
-                        Log.d("getBoard Error", e.getMessage());
-                        dialog.cancel();
+                    public void call(Throwable throwable) {
+                        Toast.makeText(MainActivity.this, "API ERROR", Toast.LENGTH_SHORT).show();
                     }
-
-                    @Override
-                    public void onNext(BoardResponseDao boardResponseDao) {
-                        for (BoardDao dao : boardResponseDao.getData()) {
-                            listSpinner.add("");
-                        }
-                    }
-                });
+                })
+                .subscribe();
     }
 
-    private void pushNotification(String topic, String title, String body) {
+    private String pushNotification(String topic, String title, String body) {
         JSONObject jPayload = new JSONObject();
         JSONObject jNotification = new JSONObject();
 //        JSONObject jData = new JSONObject();
@@ -138,10 +148,11 @@ public class MainActivity extends AppCompatActivity {
             InputStream inputStream = conn.getInputStream();
             String resp = convertStreamToString(inputStream);
 
-            Toast.makeText(this, resp, Toast.LENGTH_SHORT).show();
+            return resp;
 
         } catch (JSONException | IOException e) {
             e.printStackTrace();
+            return null;
         }
     }
 
